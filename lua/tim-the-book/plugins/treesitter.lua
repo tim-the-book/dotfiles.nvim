916920cfec4a17d2988f3bbbb1df6ae25c9ce3bb
tim-the-book <184200808+tim-the-book@users.noreply.github.com>
2026-01-07 03:40:16 +0300

    feat: change 'configs' to 'config' in treesitter; fix styling

--- lua/tim-the-book/plugins/treesitter.lua
+++ lua/tim-the-book/plugins/treesitter.lua
@@ -5,14 +5,14 @@ return {
     build = ':TSUpdate',
 
     config = function()
-        require('nvim-treesitter.configs').setup {
-	    ensure_installed = { 'c', 'python', 'rust', 'go', 'lua' },
-	    
-	    auto_install = true,
-	    sync_install = false,
-	    
-	    indent = { enable = true },
-	    highlight = { enable = true }
-	}
+        require('nvim-treesitter.config').setup {
+    	    ensure_installed = { 'c', 'python', 'rust', 'go', 'lua' },
+    	    
+    	    auto_install = true,
+    	    sync_install = false,
+    	    
+    	    indent = { enable = true },
+    	    highlight = { enable = true }
+    	}
     end,
 }
